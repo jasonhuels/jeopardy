62113c421bbceefe21e5aa1548eaf4d1f95bf820
Student <example@example.com>
2019-09-11 15:04:20 -0700

    add score for correct answer

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ import {Character} from './character.js';
 import {Player} from './player.js';
 
 $(document).ready(function() {
-  let player1;
-  let player2;
+  let player1 = new Player("player1");
+  let player2 = new Player("player2");
   const players = [player1, player2];
   let turn = Math.round(Math.random());
   let questions = [];
@@ -17,23 +17,27 @@ $(document).ready(function() {
   let promise2;
   let answerKeys = ["a1", "a2", "a3", "a4"];
   const reg_nums = /[^0-9.]/;
+  let timeInterval;
+  let timer = 0;
+
 
   //remove this after testing
-  player1 = new Player("player1");
-  player2 = new Player("player2");
+  // player1 = new Player("player1");
+  // player2 = new Player("player2");
 
   //Create Players
-  // for(let i=0; i<2; i++){
-  //   let character = new Character();
-  //   let promise1 = character.getCharacter();
-  //   promise1.then(function(response) {
-  //     const body = JSON.parse(response);
-  //     players[i] = new Player(body.name);
-  //     $(`#player${i+1}`).html(`<img src=${body.image}><br><h3>${body.name}: <span id=p${i+1}-score></span></h3>`);
-  //   }, function(error) {
-  //     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-  //   });
-  // }
+  for(let i=0; i<2; i++){
+    let character = new Character();
+    let promise1 = character.getCharacter();
+    promise1.then(function(response) {
+      const body = JSON.parse(response);
+      // players[i] = new Player(body.name);
+      players[i].name = body.name;
+      $(`#player${i+1}`).html(`<img src=${body.image}><br><h3>${body.name}: <span id=p${i+1}-score></span></h3>`);
+    }, function(error) {
+      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    });
+  }
 
   // Get first round of getQuestions
     let jeopardy = new Jeopardy();
@@ -62,31 +66,59 @@ $(document).ready(function() {
   // Buzzer
   $("html").keydown(function(e) {
       if(e.shiftKey) {
+        timeInterval=setInterval(()=> {
+          timer++;
+          if(timer >= 10){
+            clearInterval(timeInterval);
+            timer = 0;
+            document.getElementById("myModal").style.display = 'none';
+          }
+          console.log(timer);
+        },1000);
          if(event.location == 1) {
            //Player1
+           turn = 0;
            console.log('left shift');
          }
          if(event.location == 2) {
            //Player2
+           turn = 1;
            console.log('right shift');
          }
       }
   });
 
-  promise2.then(function(response) {
+  promise2.then(function() {
     $("div.question").click(function() {
+      timer = 0;
+      clearInterval(timeInterval);
       let index = this.id.replace(reg_nums, '');
       document.getElementById("myModal").style.display = "block";
       $("#jep-quest").text(questions[index]);
-      console.log(answers[index], notAnswers[index])
+      //will want to move to buzzer function.
+
+
+      console.log(answers[index], notAnswers[index]); // delete after testing
       if(notAnswers[index].length < 4) {
         notAnswers[index].splice(Math.floor(Math.random()*3), 0, answers[index]);
-
         for(let i=0; i<answerKeys.length; i++){
           $(`#${answerKeys[i]}`).html(notAnswers[index][i]);
         }
       }
+      $(".answersOptions").click(function() {
+        // if(this.innerHTML)
+        if(this.innerHTML === answers[index]) {
+          console.log("You done it!");
+          players[turn].score += 100;
+          console.log(players);
+          console.log(player1);
+          console.log(player2);
+          $("#p1-score").html(player1.score);
+          $("#p2-score").html(player2.score);
+        }
+      });
     });
+
 
   }, function(error) {
     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
